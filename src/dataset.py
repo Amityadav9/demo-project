134fe5b4f1a19d0011d0cbd174890aabfeb184ff
Amityadav9 <amit.cr44@gmail.com>
2024-09-16 16:28:08 +0200

    Docs folder added

--- src/dataset.py
+++ src/dataset.py
@@ -4,10 +4,11 @@ data = {
     "Name": ["Alice", "Bob", "Charlie", "David"],
     "Age": [24, 43, 24, 56],
     "City": ["New York", "LA", "Chicago", "Houston"],
+    "Job": ["Designer", "Engineer", "Developer", "Architect"],
 }
 
 df = pd.DataFrame(data)
 
-df.head(2)
+df.head()
 
 df = df.replace("Alice", "Alan")
